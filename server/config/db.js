dbf0476d191a23f764e4486466579d9818d51f36
John69888Canada <cliu162@my.centennialcollege.ca>
2022-10-30 10:04:21 -0500

    databse change to MongoAtlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1:27017/book_store","Secret": 'SomeSecret'
+ //   "URI": "mongodb://127.0.0.1:27017/book_store","Secret": 'SomeSecret'
+      "URI": "mongodb+srv://john69888:Hl99Hl99@cluster0.szdzazh.mongodb.net/?retryWrites=true&w=majority",
+
+ //"URI": "mongodb+srv://john69888:Hl99Hl99@cluster0.szdzazh.mongodb.net/book_store?retryWrites=true&w=majority";
    // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
     //"Secret": 'SomeSecret'
 }
